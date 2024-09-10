65ed1c6d1b312a0d97674f664a85729dd1317833
nathanwbailey <nathanbaileyw@gmail.com>
2024-09-10 12:42:34 +0100

    Update main, add train file

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ from tokenizers import BertWordPieceTokenizer
 from transformers import BertTokenizer
 import tqdm
 from pathlib import Path
+from model import BERTLM
+from model import BERT
+from dataset import BERTDataset
+import torch
+from train import train_model
+
 MAXLEN = 64
 
 CORPUS_MOVIE_CONV = './datasets/movie_conversations.txt'
 CORPUS_MOVIE_LINES = './datasets/movie_lines.txt'
+
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda:0")
+elif torch.backends.mps.is_available():
+    DEVICE = torch.device("mps")
+else:
+    DEVICE = torch.device("cpu")
 
 with open(CORPUS_MOVIE_CONV, 'r', encoding='iso-8859-1') as c:
     conv = c.readlines()
@@ -72,4 +85,26 @@ Path("./bert-it-1").mkdir(parents=True, exist_ok=True)
 tokenizer.save_model('./bert-it-1')
 bert_tokenizer = BertTokenizer.from_pretrained('./bert-it-1', local_files_only=True) #Only look for local files on machine and not from web
 
+train_data = BERTDataset(pairs, seq_len=MAXLEN, tokenizer=bert_tokenizer)
 
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True, pin_memory=True)
+
+bert_model = BERT(
+    vocab_size=len(bert_tokenizer.vocab),
+    d_model=768,
+    n_layers=2,
+    heads=12,
+    dropout=0.1
+)
+
+model = BERTLM(bert_model, len(bert_tokenizer.vocab))
+
+
+optimizer = torch.optim.Adam(
+    params=filter(lambda param: param.requires_grad, model.parameters()),
+    lr=1e-3, betas=(0.9, 0.999), weight_decay=0.01,
+)
+
+loss = torch.nn.NLLLoss(ignore_index=0)
+
+train_model(model=model, num_epochs=100, optimizer=optimizer, loss_function=loss, trainloader=trainloader, device=DEVICE)
---
+++ train.py
@@ -0,0 +1,34 @@
+import torch
+from typing import Callable
+
+
+def train_model(
+    model: torch.nn.Module,
+    num_epochs: int,
+    optimizer: torch.optim.Optimizer,
+    loss_function: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+    trainloader: torch.utils.data.DataLoader,
+    device: torch.device,
+):
+    for epoch in range(1, num_epochs+1):
+        model.train()
+        avg_loss = 0.0
+        total_correct = 0
+        total_element = 0
+        for batch in trainloader:
+            optimizer.zero_grad()
+            data = batch.to(device)
+            next_sent_output, mask_lm_output = model(data["bert_input"], data["segment_label"])
+
+            next_loss = loss_function(next_sent_output, data["is_next"])
+
+            mask_loss = loss_function(mask_lm_output.transpose(1, 2), data["bert_label"])
+            loss = next_loss + mask_loss
+            loss.backward()
+            optimizer.step()
+
+            correct = next_sent_output.argmax(dim=-1).eq(data["is_next"]).sum().item()
+            avg_loss += loss.item()
+            total_correct += correct
+            total_element += data["is_next"].nelement()
+        print(f"Epoch: {epoch}, Avg Loss: {avg_loss:.4f}, Acc: {total_correct/total_element:.4f}")
